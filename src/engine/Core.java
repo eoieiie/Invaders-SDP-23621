be1ea088889f4e12f2864532c3fcde885943e084
Kyle <dydxo0612@naver.com>
2026-09-16 19:22:44 +0900

    feat: add settings UI routing block in Core loop

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -172,6 +172,14 @@ public final class Core {
 				returnCode = frame.setScreen(currentScreen);
 				LOGGER.info("Closing high score screen.");
 				break;
+			case 4:
+				// Settings UI
+				currentScreen = new SettingScreen(width, height, FPS);
+				LOGGER.info("Starting" + WIDTH + "x" + HEIGHT
+						+ "settings screen at " + FPS + " fps.");
+				returnCode = frame.setScreen(currentScreen);
+				LOGGER.info("Closing settings screen.");
+				break;
 			default:
 				break;
 			}
